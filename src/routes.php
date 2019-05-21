aa625a6e3e26ae8d8a85d74bdbec76456254529f
m.walczak <m.walczak@inis.pl>
2019-05-21 21:21:15 +0200

    get csv with users and access google spreadsheets, first import action

--- src/routes.php
+++ src/routes.php
@@ -13,3 +13,7 @@ $app->get('/recent', AppController::class . ':recentAction')->setName('recent');
 $app->post('/tokensignin', AppController::class . ':tokenSignInAction');
 
 $app->get('/tokensignout', AppController::class . ':tokenSignOutAction');
+
+$app->get('/csv', AppController::class . ':usersAction')->setArgument('csv', 1)->setName('csv');
+
+$app->get('/import', AppController::class . ':importAction')->setName('import');
